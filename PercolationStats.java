1bc210b69be84d027c113391f698e7d9a198be67
tiffaniechia <tiffaniekyrachia@gmail.com>
2015-02-03 22:01:52 +0800

    mean and std dev calc added

--- PercolationStats.java
+++ PercolationStats.java
@@ -7,17 +7,24 @@ public class PercolationStats {
     public int randomRow;
     public int randomCol;
     public int gridSize;
-    
+    public int openSquares;
+    public int sumOfThresholds = 0;
+    public int[] thresholdArray;
+        
     public PercolationStats(int N, int T) {
         percolation = new Percolation(N);
         gridSize = N*N;
+        thresholdArray = new int[T];
         for(int i=0; i<T; i++){
+            openSquares = 0;
             while(!percolation.percolates()) {
                 randomRow = rand.nextInt(N);
                 randomCol = rand.nextInt(N);
                 percolation.open(randomRow,randomCol);
-                
+                openSquares++;
             }
+            thresholdArray[i] = openSquares/gridSize;
+            sumOfThresholds += openSquares/gridSize;
             runCount++; 
         }
     };
@@ -26,10 +33,19 @@ public class PercolationStats {
          return runCount;
     }
     
-//    public double mean() {
-//        runCount/gridSize;    
-//    }                     
-//   public double stddev()                    
+    public double mean() {
+        return sumOfThresholds/runCount;    
+    }                     
+    public double stddev() {
+        double sumOfDifferenceSquared = 0;
+        for(int i= 0; i< runCount; i++) {
+           double difference = thresholdArray[i] - mean(); 
+           sumOfDifferenceSquared += difference*difference;    
+        }
+        double variance = sumOfDifferenceSquared/runCount;
+        double stdDev = Math.sqrt(variance);
+        return stdDev;
+    }                    
 //   public double confidenceLo()              
 //   public double confidenceHi()              
    
